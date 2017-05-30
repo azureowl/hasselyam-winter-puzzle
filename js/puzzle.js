c0165cb4a53bbff5eeee9a72e844bc8ebbec797a
Azureowl <maiaroche16@gmail.com>
2017-05-30 05:36:25 -0700

    Completed drag and drop functionality

--- js/puzzle.js
+++ js/puzzle.js
@@ -6,19 +6,22 @@
         start = document.querySelector(".start"),
         puzzleCont = document.querySelector(".puzzle-container"),
         unsolvedBG = document.querySelector(".unsolved"),
-        solvedBG = document.querySelector(".solved");
-  let hints = document.querySelector(".hints-left");
+        solvedBG = document.querySelector(".solved"),
+        dropTileBoard = document.getElementById('drop-tile-board'),
+        dropTileTray = document.getElementById('drop-tile-tray'),
+        puzzleSolvedSound = document.getElementById('solved-100');
+    let hints = document.querySelector(".hints-left");
 
-  const timer = num => {
+
+  const createTimer = num => {
     const chosenTime = num,
           currMin = new Date().getMinutes(),
           endTime = new Date().setMinutes(currMin + chosenTime);
 
-    const timeRemaining = stopTime => {
+    const getTimeRemaining = stopTime => {
       const ms = endTime - Date.parse(new Date()),
             seconds = Math.floor((ms/1000) % 60),
             minutes = Math.floor((ms/1000/60) % 60);
-      // console.log(minutes, seconds);
       return {
         total: ms,
         minutes: minutes,
@@ -26,9 +29,9 @@
       };
     };
 
-    const startTimer = (el, stopTime) => {
+    const updateTimer = (el, stopTime) => {
       const runTime = () => {
-        const time = timeRemaining(endTime),
+        const time = getTimeRemaining(endTime),
               min = ("0" + time.minutes).slice(-2),
               sec = ("0" + time.seconds).slice(-2);
         clock.innerHTML = `${min}:${sec}`;
@@ -39,10 +42,98 @@
       const updateClock = setInterval(runTime, 1000);
       Window.updateClock = updateClock;
     };
-    startTimer(".time", endTime);
+    updateTimer(".time", endTime);
   };
 
-  const showHint = () => {
+
+
+  const initiateTimer = () => {
+    start.addEventListener("click",initialize);
+    function initialize () {
+      const level = selectLevel();
+      if (!isNaN(parseInt(level))) {
+        shufflePieces();
+        createTimer(parseInt(level));
+      }
+      if (level === "No Hassel") {
+        celebrate(".hassel");
+      }
+      input.setAttribute("disabled","disabled");
+      ul.classList.add("class","disabled");
+    }
+    endGame();
+  };
+
+
+
+  const startDrag = {
+    init: function () {
+      this.cacheDOM();
+      this.bindEvents();
+    },
+    cacheDOM: function () {
+      this.puzzlePieces = document.querySelectorAll('img[src*=piece]');
+    },
+    bindEvents: function () {
+      for (var i = 0; i < this.puzzlePieces.length; i++) {
+        this.puzzlePieces[i].addEventListener("mousedown", this.applyAttr);
+        this.puzzlePieces[i].addEventListener("dragstart", this.dragStart);
+      }
+    },
+    applyAttr: function (e) {
+      this.setAttribute("draggable", "true");
+    },
+    dragStart: function (e) {
+      e.dataTransfer.setData("text/plain", e.target.id);
+      e.dataTransfer.dropEffect = "move";
+    }
+  };
+
+
+
+  var validDropZones = {
+    init: function () {
+      this.cacheDOM();
+      this.bindEvents();
+    },
+    cacheDOM: function () {
+      this.tray = document.querySelector(".tray");
+      this.cell = document.getElementsByClassName("hc-cell");
+    },
+    bindEvents: function () {
+      this.tray.addEventListener("dragover", this.dragItem);
+      this.tray.addEventListener("drop", this.dropItem);
+      for (var i = 0; i < this.cell.length; i++) {
+        this.cell[i].addEventListener("dragover", this.dragItem);
+        this.cell[i].addEventListener("drop", this.dropItem);
+      }
+    },
+    dragItem: function (e) {
+      e.preventDefault();
+      e.dataTransfer.dropEffect = "move";
+    },
+    dropItem: function (e) {
+      e.preventDefault();
+      var movedPiece = e.dataTransfer.getData("text");
+      if (e.target.parentNode.id === 'tray') {
+        e.target.parentNode.appendChild(document.getElementById(movedPiece));
+        dropTileTray.play();
+      }
+      else if (movedPiece === e.target.getAttribute("data-piece")) {
+        e.target.appendChild(document.getElementById(movedPiece));
+        dropTileBoard.play();
+      }
+      else {
+        dropTileTray.play();
+      }
+      // check if the tray is empty
+      // check.checkTray();
+    }
+  };
+
+
+
+  const getHint = () => {
     let hintsLeft,
         hintsLeftText = document.querySelector(".hint-details"),
         fired = false;
@@ -63,8 +154,10 @@
       }
       hints = hints;
     });
-    document.body.addEventListener("keyup", showAfterKey);
-    function showAfterKey () {
+
+    document.body.addEventListener("keyup", hideHint);
+
+    function hideHint () {
       if (hintsLeft >= 1) {
         fired = false;
         solvedBG.classList.add("hide-bg");
@@ -72,6 +165,8 @@
       }
     }
   };
+
+
 
   const selectLevel = () => {
     const li = Array.from(document.querySelectorAll("[data-level]"));
@@ -97,22 +192,7 @@
     return selected;
   };
 
-  const initiateTimer = () => {
-    start.addEventListener("click",initialize);
-    function initialize () {
-      const level = selectLevel();
-      if (!isNaN(parseInt(level))) {
-        shufflePieces();
-        timer(parseInt(level));
-      }
-      if (level === "No Hassel") {
-        celebrate(".hassel");
-      }
-      input.setAttribute("disabled","disabled");
-      ul.classList.add("class","disabled");
-    }
-    endGame();
-  };
+
 
   const shufflePieces = () => {
     const pieces = Array.from(document.querySelectorAll("[data-piece]")),
@@ -126,8 +206,9 @@
       piece.style.left = `${left}%`;
       piece.style.top = `${top}%`;
     }
-
   };
+
+
 
   const endGame = () => {
     stop.addEventListener("click",endGame);
@@ -139,6 +220,8 @@
       ul.classList.remove("class","disabled");
     }
   };
+
+
 
   const celebrate = (className) => {
     const image = document.querySelector(className),
@@ -162,7 +245,11 @@
     stop.addEventListener("click",endFun);
   };
 
-  showHint();
+
+
+  getHint();
   initiateTimer();
   selectLevel();
+  startDrag.init();
+  validDropZones.init();
 }());
